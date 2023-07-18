af0ac934021235fe3825300283379760126f8926
Abdalrahman <abdalrahmandaas963@gmail.com>
2023-07-18 18:18:53 +0300

    first

--- App.js
+++ App.js
@@ -1,20 +1,126 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Icon from "react-native-vector-icons/FontAwesome";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+import Button from "./src/components/Button";
+import Row from "./src/components/Row";
+import calculator, { initialState } from "./util/calculator";
+
+// create class component of App
+export default class App extends Component {
+  state = initialState;
+
+  // handle tap method
+  HandleTap = (type, value) => {
+    this.setState((state) => calculator(type, value, state));
+  };
+
+  // render method
+  render() {
+    return (
+      <View style={styles.container}>
+        {/* Status bae here */}
+        <SafeAreaView>
+          <Text style={styles.value}>
+            {parseFloat(this.state.currentValue).toLocaleString()}
+          </Text>
+
+          {/* Do create componentRow */}
+          <Row>
+            <Button
+              text="C"
+              theme="secondary"
+              onPress={() => this.HandleTap("clear")}
+            />
+
+            <Button
+              text="+/-"
+              theme="secondary"
+              onPress={() => this.HandleTap("posneg")}
+            />
+
+            <Button
+              text="%"
+              theme="secondary"
+              onPress={() => this.HandleTap("percentage")}
+            />
+
+            <Button
+              theme="primary"
+              onPress={() => this.HandleTap("deleteLastNumber", "del")}
+            >
+              <Ionicons name="backspace" size={24} color="white" />
+            </Button>
+          </Row>
+
+          {/* Number */}
+          <Row>
+            <Button text="7" onPress={() => this.HandleTap("number", 7)} />
+            <Button text="8" onPress={() => this.HandleTap("number", 8)} />
+            <Button text="9" onPress={() => this.HandleTap("number", 9)} />
+            <Button
+              text="X"
+              theme="accent"
+              onPress={() => this.HandleTap("operator", "*")}
+            />
+          </Row>
+
+          <Row>
+            <Button text="5" onPress={() => this.HandleTap("number", 5)} />
+            <Button text="6" onPress={() => this.HandleTap("number", 6)} />
+            <Button text="7" onPress={() => this.HandleTap("number", 7)} />
+            <Button
+              text="-"
+              theme="accent"
+              onPress={() => this.HandleTap("operator", "-")}
+            />
+          </Row>
+
+          <Row>
+            <Button text="1" onPress={() => this.HandleTap("number", 1)} />
+            <Button text="2" onPress={() => this.HandleTap("number", 2)} />
+            <Button text="3" onPress={() => this.HandleTap("number", 3)} />
+            <Button
+              text="+"
+              theme="accent"
+              onPress={() => this.HandleTap("operator", "+")}
+            />
+          </Row>
+
+          <Row>
+            <Button text="0" onPress={() => this.HandleTap("number", 0)} />
+            <Button text="." onPress={() => this.HandleTap("number", ".")} />
+            <Button
+              text="="
+              theme="primary"
+              onPress={() => this.HandleTap("equal", "=")}
+            />
+            <Button
+              text="/"
+              theme="accent"
+              onPress={() => this.HandleTap("operator", "/")}
+            />
+          </Row>
+        </SafeAreaView>
+      </View>
+    );
+  }
 }
 
+// create styles of app
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#B7D3EE",
+    justifyContent: "flex-end",
+  },
+  value: {
+    color: "#fff",
+    fontSize: 42,
+    textAlign: "right",
+    marginRight: 20,
+    marginBottom: 10,
   },
 });
---
+++ util/calculator.js
@@ -0,0 +1,94 @@
+export const initialState = {
+  currentValue: "0",
+  operator: null,
+  previousValue: null,
+};
+
+export const handleNumber = (value, state) => {
+  if (state.currentValue === "0") {
+    return { currentValue: `${value}` };
+  }
+
+  return {
+    currentValue: `${state.currentValue}${value}`,
+  };
+};
+
+const handleDeleteLastNumber = (state) => {
+  // Get the current value and remove the last character
+  const currentValue = state.currentValue.slice(0, -1);
+
+  // If the current value becomes empty, set it back to "0"
+  if (currentValue === "") {
+    return { currentValue: "0" };
+  }
+
+  return { currentValue };
+};
+
+const handleEqual = (state) => {
+  const { currentValue, previousValue, operator } = state;
+
+  const current = parseFloat(currentValue);
+  const previous = parseFloat(previousValue);
+  const resetState = { operator: null, previousValue: null };
+
+  switch (operator) {
+    case "+":
+      return {
+        currentValue: `${previous + current}`,
+        ...resetState,
+      };
+    case "-":
+      return {
+        currentValue: `${previous - current}`,
+        ...resetState,
+      };
+    case "*":
+      return {
+        currentValue: `${previous * current}`,
+        ...resetState,
+      };
+    case "/":
+      return {
+        currentValue: `${previous / current}`,
+        ...resetState,
+      };
+
+    default:
+      return state;
+  }
+};
+
+// calculator function
+const calculator = (type, value, state) => {
+  switch (type) {
+    case "number":
+      return handleNumber(value, state);
+    case "clear":
+      return initialState;
+    case "posneg":
+      return {
+        currentValue: `${parseFloat(state.currentValue) * -1}`,
+      };
+    case "percentage":
+      return {
+        currentValue: `${parseFloat(state.currentValue) * 0.01}`,
+      };
+    case "operator":
+      return {
+        operator: value,
+        previousValue: state.currentValue,
+        currentValue: "0",
+      };
+    case "equal":
+      return handleEqual(state);
+    case "deleteLastNumber":
+      return handleDeleteLastNumber(state);
+
+    default:
+      return state;
+  }
+};
+
+export default calculator;
